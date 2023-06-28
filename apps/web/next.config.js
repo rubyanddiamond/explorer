ec32b2adda8e47ccf6c2773e32a86a7659fc8879
Amnot <acyclicgraphs@gmail.com>
2023-06-28 10:49:37 +0000

    add webpack to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,4 +7,16 @@ module.exports = {
   images: {
     domains: ['nautchain.xyz', 'mc-nft.s3.us-west-2.amazonaws.com', 'ucarecdn.com'],
   },
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      // Fix "Can't resolve 'https'" error by providing an empty module
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        https: false,
+      };
+    }
+
+    return config;
+  },
 };
